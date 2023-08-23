d938b4cbd20ca2dd8a2ace2d2a60edd705ef9ae5
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2023-08-23 22:32:00 +0900

    :wrench: Add configuration module

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
+import configuration from "./config/configuration";
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    ignoreEnvFile: true,
+    isGlobal: true,
+    load: [configuration],
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
